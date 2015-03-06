1ac7aa76566318a32dafa8d25c1a84669fd5bad8
Conner D <connerdunn7399@gmail.com>
2015-03-05 17:23:29 -0700

    start.py creates tbls and pops them

--- start.py
+++ start.py
@@ -12,26 +12,51 @@ con = cx_Oracle.connect(user+"/"+passw+"@gwynne.cs.ualberta.ca:1521/CRS")  # let
 
 curs = con.cursor()
 
-# drop tables if they exist
-statement = "drop table movie"
-curs.execute(statement)
+# old demo code
 
-# create table statements
-statement = "create table movie(title char(20), movie_number integer, primary key(movie_number))"
-curs.execute(statement)
-
-# insert data into tables
-statement = "insert into movie values('Chicago', 1)"
-curs.execute(statement)
-
-# make a selection from the data
-query = "select title, movie_number from movie"
-curs.execute(query)
-rows = curs.fetchall()
+# # drop tables if they exist
+# statement = "drop table movie"
+# curs.execute(statement)
+# 
+# # create table statements
+# statement = "create table movie(title char(20), movie_number integer, primary key(movie_number))"
+# curs.execute(statement)
+# 
+# # insert data into tables
+# statement = "insert into movie values('Chicago', 1)"
+# curs.execute(statement)
+# 
+# # make a selection from the data
+# query = "select title, movie_number from movie"
+# curs.execute(query)
+# rows = curs.fetchall()
 
 # close connection and curs
+
+# Drop & Create Tables
+print("Dropping / Creating Tables")
+f = open("p1_setup.sql.txt")
+tables = f.read().replace("\n", "").split(";")
+for t in tables:
+	# print(t)
+	if (len(t) <= 0):
+		continue
+	curs.execute(t)
+f.close()
+
+# Populate tables
+print("Populate Tables")
+f = open("population.txt")
+pop = f.read().replace("\n", "").split(";")
+for t in pop:
+	# print(t)
+	if (len(t) <= 0):
+		continue
+	curs.execute(t)
+f.close()
+
 curs.close()
 con.close()
-[
+
 # print the selection
-print(rows)
+# print(rows)
