e41fad518cf78993688c8746edd663dd398c7557
sbaergen <sbaergen@ualberta.ca>
2015-03-09 12:31:35 -0600

    Added main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is the main menu.
+# This will display the menu and handle input to the menu
+
+print ("Welcome to the Alberta Auto Registration System!")
+print ("----------------------------------------")
+print ("""Please Select from the following:
+	1:New Vehicle Registration
+	2:Auto Transaction
+	3:Driver Licence Registration
+	4:Violation Record
+	5:Search Engine
+	6:Exit""")
+choice = input("Choice (1-6): ")
+
+print (choice)
