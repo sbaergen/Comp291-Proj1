ee774746334467fb41a9d0cde131195d2ab9f495
Conner D <connerdunn7399@gmail.com>
2015-03-14 22:15:30 -0600

    some refactoring and var name changes

--- main.py
+++ main.py
@@ -3,137 +3,135 @@ import sql as sqlFile
 
 # This will display the menu and handle input to the menu
 def main():
-	print("Please login before proceeding.")
-	user = input("User [%s]:" % getpass.getuser())
-	if not user:
-		user = getpass.getuser()
-	passw = getpass.getpass("Pass:")
+        print("Please login before proceeding.")
+        user = input("User [%s]:" % getpass.getuser())
+        if not user:
+                user = getpass.getuser()
+                passw = getpass.getpass("Pass:")
 
-	# create a new instance of a connection object
-	sql = sqlFile.SqlConnection(user, passw)
+        # create a new instance of a connection object
+        sql = sqlFile.SqlConnection(user, passw)
 
-	# Drop & Create Tables
-	print("Dropping / Creating Tables")
-	sql.executeFromFile("p1_setup.sql.txt")
+        # Drop & Create Tables
+        print("Dropping / Creating Tables")
+        sql.executeFromFile("p1_setup.sql.txt")
 
-	# Populate tables
-	print("Populate Tables")
-	sql.executeFromFile("population.txt")
+        # Populate tables
+        print("Populate Tables")
+        sql.executeFromFile("population.txt")
 
-	print ("Welcome to the Alberta Auto Registration System!")
-	choice = 7
-	while (choice != 6):
-		print ("----------------------------------------")
-		print ("""Please Select from the following:
-		1:New Vehicle Registration
-		2:Auto Transaction
-		3:Driver Licence Registration
-		4:Violation Record
-		5:Search Engine
-		6:Exit""")
-		choice = 7
-		while (choice == 7):
-			try:
-				choice = eval(input("Choice (1-6): "))
-				if (not (choice >= 1 and choice <=6)):
-					choice = 7  # Will cause a loop back to get new entry (choice was invalid)
-					print ("Invalid Input!", end = " ")
-			except:
-				choice = 7  # Will cause a loop back to get new entry (choice was invalid)
-				print ("Invalid Input!", end = " ")
-				
-		if choice == 1:
-			newVehicle(sql)
-		elif choice == 2:
-			autoTrans(sql)
-		elif choice == 3:
-			licenceReg(sql)
-		elif choice == 4:
-			violationRec(sql)
-		elif choice == 5:
-			searchEngine(sql)
-		elif choice == 6:
-			break
-		else:
-			choice = 7  # Will cause a loop back to get new entry (choice was invalid)
-			print ("Invalid Input!", end = " ")
-		print (choice)
+        print ("Welcome to the Alberta Auto Registration System!")
+        choice = 7
+        while (choice != 6):
+                print ("----------------------------------------")
+                print ("""Please Select from the following:
+                1:New Vehicle Registration
+                2:Auto Transaction
+                3:Driver Licence Registration
+                4:Violation Record
+                5:Search Engine
+                6:Exit""")
+                choice = 7
+                while (choice == 7):
+                        try:
+                                choice = eval(input("Choice (1-6): "))
+                                if (not (choice >= 1 and choice <=6)):
+                                        choice = 7  # Will cause a loop back to get new entry (choice was invalid)
+                                        print ("Invalid Input!", end = " ")
+                        except:
+                                choice = 7  # Will cause a loop back to get new entry (choice was invalid)
+                                print ("Invalid Input!", end = " ")
 
-	sql.close()  # clean up sql object
+                if choice == 1:
+                        newVehicle(sql)
+                elif choice == 2:
+                        autoTrans(sql)
+                elif choice == 3:
+                        licenceReg(sql)
+                elif choice == 4:
+                        violationRec(sql)
+                elif choice == 5:
+                        searchEngine(sql)
+                elif choice == 6:
+                        break
+                else:
+                        choice = 7  # Will cause a loop back to get new entry (choice was invalid)
+                        print ("Invalid Input!", end = " ")
+                        print (choice)
+
+        sql.close()  # clean up sql object
 
 
 
-#Register new vehicle by officer. All detailed information about the vehicle and personal information about the owner. 
-#You may assume all vehicle types have been loaded into the inital database.
+# Register new vehicle by officer. All detailed information about the vehicle and personal information about the owner. 
+# You may assume all vehicle types have been loaded into the inital database.
+# Create a new vehicle and select owner, if no owner exists create a new person
 def newVehicle(sql):
-	print("NEW VEHICLE REGISTRATION")
-	print("")
-	serial_no = input("Enter serial_no of vehicle: ")
-	maker = input("Enter the make of the vehicle: ")
-	model = input("Enter the model of the vehicle: ")
-	year = input("Enter the year of the vehicle: ")
-	color = input("Enter the color of the vehicle: ")
-	vehicleType = eval(input("Enter the type of the vehicle (1=car,2=suv,3=crossover,4=van,5=truck): "))
+        print("NEW VEHICLE REGISTRATION")
+        print("")
+        serial_no = input("Enter serial_no of vehicle: ")
+        maker = input("Enter the make of the vehicle: ")
+        model = input("Enter the model of the vehicle: ")
+        year = input("Enter the year of the vehicle: ")
+        color = input("Enter the color of the vehicle: ")
+        vehicleType = eval(input("Enter the type of the vehicle (1=car,2=suv,3=crossover,4=van,5=truck): "))
 
-	# todo: lets check all of the params
+        # todo: lets check all of the params
 
-	string = "insert into vehicle values('{:s}','{:s}','{:s}','{:s}','{:s}',{:d})"
-	string = string.format(serial_no, maker, model, year, color, vehicleType)
-	print(string)  # debugging
-	sql.execute(string)
+        string = "insert into vehicle values('{:s}','{:s}','{:s}','{:s}','{:s}',{:d})"
+        sql.execute(string.format(serial_no, maker, model, year, color, vehicleType))
 
-	print("past part one")  # debugging
+        addOwner = True
+        while addOwner:
+                Owner = input("Enter the owner id of the owner of the vehicle: ")
+                string = "SELECT o.owner_id FROM owner o WHERE o.owner_id = {:s}".format(Owner)
 
-	Continue = True
-	while Continue: # todo: the owner doesn't exist because we just created this vehicle out of thin air... fix this...
-		Owner = input("Enter the owner id of the owner of the vehicle: ")
-		temp = sql.exeAndFetch("SELECT o.owner_id FROM owner o, WHERE o.owner_id = {:s} and o.vehicle_id = {:s}".format(Owner, serial_no))
-		print(str(type(temp)) + " " + str(temp))  # debugging
-		print(temp)  # debugging
-		if len(temp) == 0:  # todo: this line isn't working correctly.. what is the ret val?
-			Primary_Ownership = input("Is this person the primary owner of the vehicle? (y/n): ")
-			# Insert into owner values(Owner,Serial_no,Primary_Ownership);
-			string = "insert into vehicle values ('{:s}','{:s}','{:s}')"
-			string = string.format(Owner, serial_no, Primary_Ownership)
-			sql.execute(string)
+                if len(sql.exeAndFetch(string)) == 0:  # check if the owner exists, if not add them
+                        string = "insert into vehicle values ('{:s}','{:s}','{:s}')"
+                        string = string.format(Owner, serial_no, Primary_Ownership)
+                        sql.execute(string)
 
-			if (sql.exeAndFetch("Select * FROM person p WHERE p.sin = {:s}").format(Owner)) == 0:
-				Sin = input("Enter the sin of the owner: ")
-				Name = input("Enter the name of the owner: ")
-				Height = input("Enter the height of the owner: ")
-				Weight = input("Enter the weight of the owner: ")
-				Eyecolor = input("Enter the eye color of the owner: ")
-				Haircolor = input("Enter the hair color of the owner: ")
-				Address = input("Enter the address of the owner: ")
-				Gender = input("Enter the gender of the owner: ")
-				Birthday = input("Enter the birthday of the owner: ")
-				string = "Insert into people values ({:s},{:s},{:s},{:s},{:s},{:s},{:s},{:s},{:s})"
-				sql.execute(string.format(Sin,Name,Height,Weight,Eyecolor,Haircolor,Address,Gender,Birthday))
+                        if (sql.exeAndFetch("Select * FROM person p WHERE p.sin = {:s}").format(Owner)) == 0:  # check if the person exists
+                                Sin = input("Enter the sin of the owner: ")
+                                Name = input("Enter the name of the owner: ")
+                                Height = input("Enter the height of the owner: ")
+                                Weight = input("Enter the weight of the owner: ")
+                                Eyecolor = input("Enter the eye color of the owner: ")
+                                Haircolor = input("Enter the hair color of the owner: ")
+                                Address = input("Enter the address of the owner: ")
+                                Gender = input("Enter the gender of the owner: ")
+                                Birthday = input("Enter the birthday of the owner: ")
+                                string = "Insert into people values ({:s},{:s},{:s},{:s},{:s},{:s},{:s},{:s},{:s})"
+                                sql.execute(string.format(Sin,Name,Height,Weight,Eyecolor,Haircolor,Address,Gender,Birthday))
 
-				Done = False
-				while not Done:
-					Continue = input("Add another owner? (y/n): ")
-					if Continue == 'y' or Continue == 'Y':
-						Continue = True
-						Done = True
-					elif Continue =='n' or Continue == 'N':
-						Continue = False
-						Done = True
-					else:
-						print("Invalid input, please enter either the letter y or n")
-		else:
-			Continue = False
+                # need to check here if the vehicle already has an owner
+                Primary_Ownership = input("Is this person the primary owner of the vehicle? (y/n): ")
+                # check for valid input of y or n
+                # Insert into owner values(Owner,Serial_no,Primary_Ownership);  # todo: sql statement not actually exicuted
+
+
+                validInput = False
+                while not Done:
+                        addMore = input("Add another owner? (y/n): ")
+                        if lower(addMore) == 'y':
+                                addOwner = True
+                                validInput = True
+                        elif lower(addMore) =='n':
+                                addOwner = False
+                                validInput = True
+                        else:
+                                print("Invalid input, please enter either the letter y or n")
 
 def autoTrans(sql):
-	return
+        return
 
 def licenceReg(sql):
-	return
+        return
 
 def violationRec(sql):
-	return
+        return
 
 def searchEngine(sql):
-	return
+        return
 
 main()  # run the main function
