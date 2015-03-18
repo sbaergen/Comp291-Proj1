9755bc0bb7b8e59d0f009dfd7c17021988c69358
sbaergen <sbaergen@ualberta.ca>
2015-03-18 13:30:48 -0600

    Finished licenceReg. Photo part is commented out and instead replaced with a hex number. Need to load picture into file system to test.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import getpass
 import sql as sqlFile
+import os,sys
 #import Image
 
 # This will display the menu and handle input to the menu
@@ -119,51 +120,58 @@ def newVehicle(sql):
                                 print("Invalid input, please enter either the letter y or n")
 
 def autoTrans(sql):
-    Vehicle = input("Enter the serial_no of the vehicle in the auto transaction: ")
-    Buyer = input("Enter the sin of the buyer: ")
-    Second_Buyer = input("Would you like to enter a second Buyer? (y/n) : ")
-    Seller = input("Enter the sin of the seller: ")
-    Date = input("Enter the date of the transaction: ")
-    Price = input("Enter the price the vehicle was sold for ($): ")
+	Vehicle = input("Enter the serial_no of the vehicle in the auto transaction: ")
+	Buyer = input("Enter the sin of the buyer: ")
+	Second_Buyer = input("Would you like to enter a second Buyer? (y/n) : ")
+	Seller = input("Enter the sin of the seller: ")
+	Date = input("Enter the date of the transaction: ")
+	Price = input("Enter the price the vehicle was sold for ($): ")
 	
-    string = "SELECT MAX(transaction_id) FROM auto_sale s"
-    TransactionId = sql.exeAndFetch(string)[0][0] + 1
+	string = "SELECT MAX(transaction_id) FROM auto_sale s"
+	TransactionId = sql.exeAndFetch(string)[0][0] + 1
 
-    string = "insert into auto_sale values({:d},'{:s}','{:s}','{:s}',TO_DATE('{:s}', 'YYYY-MM-DD'), '{:s}')"
-    string = string.format(TransactionId, Seller, Buyer, Vehicle, Date, Price)
-    print(string)
-    sql.execute(string)
+	string = "insert into auto_sale values({:d},'{:s}','{:s}','{:s}',TO_DATE('{:s}', 'YYYY-MM-DD'), '{:s}')"
+	string = string.format(TransactionId, Seller, Buyer, Vehicle, Date, Price)
+	print(string)
+	sql.execute(string)
 #This line crashes
-    string = "delete from owner where (owner_id != {:s} and vehicle_id = {:s})".format(Buyer, Vehicle)
-    print(string)
-    sql.execute(string)
+	string = "insert into owner values('{:s}','{:s}','{:s}')".format('112112112', '1001', 'y')
+	sql.execute(string)
+	string = "delete from owner where (owner_id != {:s} and vehicle_id = {:s})"
+	string = string.format(Buyer, Vehicle)
+	print(string)
+	sql.execute(string)
 #Method 1
-    string = "insert into owner values('{:s}','{:s}','{:s}')"
-    string = string.format(Buyer, Vehicle, 'Y')
-    sql.execute(string)	
-    return
+	string = "insert into owner values('{:s}','{:s}','{:s}')"
+	string = string.format(Buyer, Vehicle, 'Y')
+	sql.execute(string)	
+	return
 
 def licenceReg(sql):
-    string = "SELECT MAX(d.licence_no) FROM drive_licence d)"
-    Licence_no = 1 + sql.exeAndFetch(string)
+	string = "SELECT MAX(licence_no) FROM drive_licence"
+	print (string)
+	Licence_no = int(sql.exeAndFetch(string)[0][0]) + 1
+	print(Licence_no)
     
-    Person = input("Enter the sin of the person: ")
-    Class = input("Enter the class of driving licence of the person: ")
-    Issuing_date = input("Enter the date of issue: ")
-    Expiry_date = input("Enter the date of expiry: ")
-    File_name = input("Enter the name of the picture file to be added: ")    
-    
-    Picture = Image.open(File_name)
-
-    string = "insert into drive_licence values ({:d},'{:s}','{:s}', '{:s}', TO_DATE('{:s}', 'YYYY-MM-DD'), TO_DATE('{:s}', 'YYYY-MM-DD'))"
-    string = string.format(Licence_no,Person,Class,Picture,Issuing_date,Expiry_date)
-    sql.execute(string)
-    return
+	Person = input("Enter the sin of the person: ")
+	Class = input("Enter the class of driving licence of the person: ")
+	Issuing_date = input("Enter the date of issue: ")
+	Expiry_date = input("Enter the date of expiry: ")
+	File_name = input("Enter the name of the picture file to be added: ")    
+# From http://stackoverflow.com/questions/4664343/open-file-in-python-and-read-bytes 18/03/15	
+	#Picture = open(File_name, "rb")
+	#Picture = Picture.read(16)
+	#print "%s" % (binascii.hexlify(Picture))
+	Picture = 0xFF
+	string = "insert into drive_licence values ({:d},'{:s}','{:s}', '{:d}', TO_DATE('{:s}', 'YYYY-MM-DD'), TO_DATE('{:s}', 'YYYY-MM-DD'))"
+	string = string.format(Licence_no,Person,Class,Picture,Issuing_date,Expiry_date)
+	sql.execute(string)
+	return
 
 def violationRec(sql):
-        return
+	return
 
 def searchEngine(sql):
-        return
+	return
 
 main()  # run the main function
